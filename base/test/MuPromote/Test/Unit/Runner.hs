e6bd65e6817b78b97ae0dd81ed321891131f3474
Philip Lykke Carlsen <plcplc@gmail.com>
2014-10-31 20:07:52 +0000

    hspec interface changes

--- base/test/MuPromote/Test/Unit/Runner.hs
+++ base/test/MuPromote/Test/Unit/Runner.hs
@@ -3,12 +3,12 @@ module MuPromote.Test.Unit.Runner (runSpec) where
 import Control.Monad (unless)
 import System.Exit (exitFailure)
 import Test.Hspec (Spec)
-import Test.Hspec.Runner (Config(..), defaultConfig, hspecWith, Summary(..))
+import Test.Hspec.Runner (Config(..), defaultConfig, hspecWithResult, Summary(..))
 
 -- | The hspec test runner.
 runSpec :: Spec -> IO ()
 runSpec specs = do
-  r <- hspecWith conf specs
+  r <- hspecWithResult conf specs
   unless (summaryFailures r == 0) exitFailure
 
 -- | The customised configuration.
